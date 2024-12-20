138aaa4dd396b5dff5f27d777bc4a7f4ca2eeae2
Alexander Weigelt <webdesign@alexander-weigelt.de>
2024-12-20 17:46:31 +0100

    Add todo-list unit test

--- apps/todo-app/src/components/todo-list/test/todo-list.spec.tsx
+++ apps/todo-app/src/components/todo-list/test/todo-list.spec.tsx
@@ -1,18 +1,30 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { TodoList } from '../todo-list';
+import { h } from '@stencil/core';
 
-describe('todo-list', () => {
-  it('renders', async () => {
+describe('ToDoList Component', () => {
+  const mockTodos = [
+    { id: 1, text: 'Learn Stencil', dueDate: '20.12.2024, 17:40:45', completed: false },
+    { id: 2, text: 'Write Tests', dueDate: '24.12.2024, 18:45:48', completed: true },
+  ];
+
+  it('matches the snapshot with todos', async () => {
     const page = await newSpecPage({
-      components: [TodoList],
-      html: `<todo-list></todo-list>`,
+      components: [],
+      template: () => <TodoList todos={mockTodos} toggleTodo={() => {
+      }} />,
     });
-    expect(page.root).toEqualHtml(`
-      <todo-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </todo-list>
-    `);
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('matches the snapshot with an empty todos array', async () => {
+    const page = await newSpecPage({
+      components: [],
+      template: () => <TodoList todos={[]} toggleTodo={() => {
+      }} />,
+    });
+
+    expect(page.root).toMatchSnapshot();
   });
 });
